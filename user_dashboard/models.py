e60df09be797e8068eee78b2e8ced407513e7b26
rishabhDev2700 <rishavkumar2700@gmail.com>
2022-07-15 23:59:35 +0530

    Refractored models file and added login required decorator

--- user_dashboard/models.py
+++ user_dashboard/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-
-# Create your models here.
 from authentication.models import CustomUser
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=50,blank=False)
-    description = models.TextField(max_length=200,blank=False)
+    title = models.CharField(max_length=50, blank=False)
+    description = models.TextField(max_length=200, blank=False)
     target = models.IntegerField(blank=False)
-    goals = models.TextField(max_length=200,blank=False)
-    owner = models.OneToOneField(to=CustomUser,on_delete=models.DO_NOTHING)
+    goals = models.TextField(max_length=200, blank=False)
+    owner = models.OneToOneField(to=CustomUser, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return f'Title:{self.title}, target:{self.target}, owner:{self.owner}'
