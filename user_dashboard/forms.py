6c2c9e0a919ca69a1882372c95ce5c8fc44b32db
rishabhDev2700 <rishavkumar2700@gmail.com>
2022-07-15 22:52:05 +0530

    created Project form

--- user_dashboard/forms.py
+++ user_dashboard/forms.py
@@ -1,9 +1,8 @@
 from django.forms import ModelForm
-
 from user_dashboard.models import Project
 
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ('title','description','target','goals')
+        fields = ('title', 'description', 'target', 'goals')
