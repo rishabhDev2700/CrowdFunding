9cf04d5c522ae537c89c18d3ca5ce98548a4334f
rishabhDev2700 <rishavkumar2700@gmail.com>
2022-07-15 20:51:28 +0530

    Created User Profile model

--- authentication/models.py
+++ authentication/models.py
@@ -5,3 +5,12 @@ from django.db import models
 # Create your models here.
 class CustomUser(AbstractUser):
     pass
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    bio = models.TextField(max_length=200, default="")
+    occupation = models.CharField(max_length=50, default="")
+    picture = models.ImageField(upload_to='users/%Y/%m', blank=True)
+    contact = models.CharField(max_length=12, blank=True)
+    loaction = models.TextField(max_length=100, blank=True)
