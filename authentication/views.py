ee765e1a5b36cb291759eca71fefa3895fa8b5f9
rishabhDev2700 <rishavkumar2700@gmail.com>
2022-07-15 22:51:26 +0530

    added login, logout and register functions

--- authentication/views.py
+++ authentication/views.py
@@ -1,19 +1,30 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
-
-
-# Create your views here.
-from authentication.models import CustomUser
+from django.urls import reverse
+from authentication.forms import CustomUserCreationForm
 
 
 def login_user(request):
-    url = CustomUser.get()
-    return render(request,'base.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return reverse('dashboard')
+    return render(request, 'authentication/login.html')
 
 
 def logout_user(request):
-    return render(request,'base.html')
+    logout(request)
+    return render(request, 'authentication/login.html')
 
 
 def register_user(request):
-    return render(request,'base.html')
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'authentication/login.html')
+    form = CustomUserCreationForm()
+    return render(request, 'base.html', {'form': form})
